d720722f6d614bad9e1879969e5202bc1edd13d2
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-09-22 15:12:41 -0300

    construcao do corpo dos componentes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
+
 export default function App() {
 	return (
-		<div className="App">
-			<h1 className="text-3xl font-bold underline">
-				Hello world!
-			</h1>
+		<div className="min-h-screen">
+			<div className="gradient-bg-welcome">
+				<Navbar />
+				<Welcome />
+			</div>
+			<Services />
+			<Transactions />
+			<Footer />
 		</div>
 	)
 }
