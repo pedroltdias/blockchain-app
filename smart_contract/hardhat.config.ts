a4b2e5723ce49cfdd183f23848751c72f1a1eb08
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-09-23 16:45:45 -0300

    conficuracao do hardhat.config.ts

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,8 +1,12 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("@nomiclabs/hardhat-waffle");
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
-
-export default config;
+module.exports = {
+	solidity: "0.8.0",
+	networks: {
+		goerli: {
+			url: "https://eth-goerli.g.alchemy.com/v2/qZGHGF5SnqY1VzOzfWJOg-EEXWCErI2f",
+			accounts: ["fb598c1c0521d9f1453f02902cd3a9c7dee4be47191f982ebec5b14f3b5fe93d"]
+		}
+	}
+}
