51752677ca99844017aea78b579dc0c86d642870
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-09-27 17:02:28 -0300

    implementacao dos gifs baseado nas chaves das transacoes

--- client/src/components/Transactions.tsx
+++ client/src/components/Transactions.tsx
@@ -4,6 +4,7 @@ import { TransactionContext } from "../context/TransactionContext";
 
 import dummyData from "../utils/dummyData";
 import { shortenAddress } from "../utils/shortenAddress";
+import useFetch from "../hooks/useFetch";
 
 interface ITransactionCard {
 	id: number,
@@ -16,6 +17,8 @@ interface ITransactionCard {
 }
 
 const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }: ITransactionCard) => {
+	const gifUrl = useFetch({ keyword });
+
 	return (
 		<div className="bg-[#181918] m-4 flex flex-1
 		 2xl:min-w-[450px]
@@ -40,9 +43,15 @@ const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, 
 						</>
 					)}
 
-					<div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
-						<p className="text-[#37c7da] font-bold">{timestamp}</p>
-					</div>
+				</div>
+				<img
+					src={gifUrl || url}
+					alt="gif"
+					className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover"
+				/>
+
+				<div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+					<p className="text-[#37c7da] font-bold">{timestamp}</p>
 				</div>
 			</div>
 		</div>
