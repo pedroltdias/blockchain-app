00098473538c51ea12842f69587d4c96b46f72d7
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-09-29 15:33:50 -0300

    construcao da funcao 'getAllTransactions()'

--- client/src/context/TransactionContext.tsx
+++ client/src/context/TransactionContext.tsx
@@ -24,10 +24,36 @@ export const TransactionProvider = ({ children }: any) => {
 	const [formData, setFormData] = useState({ addressTo: "", amount: "", keyword: "", message: "" });
 	const [isLoading, setIsLoading] = useState(false);
 	const [transactionCount, setTransactionCount] = useState(localStorage.getItem('transactionCount'));
+	const [transactions, setTransactions] = useState([]);
 
 	const handleChange = (e: any, name: any) => {
 		setFormData((prevState) => ({ ...prevState, [name]: e.target.value }));
-	};
+	}
+
+	const getAllTransactions = async () => {
+		try {
+			if (!ethereum) return alert("Please install metamask!");
+			const transactionContract = getEthereumContract();
+
+			const availableTransactions = await transactionContract.getAllTransactions();
+
+			const structuredTransactions = availableTransactions.map((transaction) => ({
+				addressTo: transaction.receiver,
+				addressFrom: transaction.sender,
+				timestamp: new Date(transaction.timestamp.toNumber() * 1000).toLocaleString(),
+				message: transaction.message,
+				keyword: transaction.keyword,
+				amount: parseInt(transaction.amount._hex) / (10 ** 18)
+			}));
+
+			console.log(structuredTransactions);
+
+			setTransctions(structuredTransactions);
+
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
 	const checkIfWalleyIsConnected = async () => {
 		try {
@@ -38,7 +64,7 @@ export const TransactionProvider = ({ children }: any) => {
 			if (accounts.length) {
 				setCurrentAccount(accounts[0]);
 
-				//getAllTransactions();
+				getAllTransactions();
 			} else {
 				console.log("No accounts found");
 			}
